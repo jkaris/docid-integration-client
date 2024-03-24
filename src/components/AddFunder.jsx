7cf7cb8e192b6c21452b52a7bc2d85441e709b3e
Joseph Kariuki <joehene@gmail.com>
2024-03-24 05:18:16 +0300

    update funder and projects section -code formatting

--- src/components/AddFunder.jsx
+++ src/components/AddFunder.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from "react";
 import { Form, Row, Col, Button } from 'react-bootstrap';
 import Select from 'react-select';
+import { PublicationFormContext } from "./context/PublicationFormContext.jsx";
 
 const AddFunder = () => {
     const [rows, setRows] = useState([{ type: '', name: '', id: '', activity: '' }]);
@@ -18,46 +19,76 @@ const AddFunder = () => {
     return (
         <div>
             {rows.map((row, index) => (
-                <Row key={index} className="m-3">
-                    <Col>
-                        <Select
-                            options={[{ value: 'funder', label: 'AddFunder' }, { value: 'grantor', label: 'Grantor' }]}
-                            placeholder="Select Type"
-                            onChange={(selectedOption) => handleInputChange(selectedOption, 'type', index)}
-                        />
-                    </Col>
-                    <Col>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter Name"
-                            value={row.name}
-                            onChange={(e) => handleInputChange(e.target.value, 'name', index)}
-                        />
-                    </Col>
-                    <Col>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter ID"
-                            value={row.id}
-                            onChange={(e) => handleInputChange(e.target.value, 'id', index)}
-                        />
-                    </Col>
-                    <Col>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter Activity"
-                            value={row.activity}
-                            onChange={(e) => handleInputChange(e.target.value, 'activity', index)}
-                        />
-                    </Col>
-                    <Col>
-                        {index === rows.length - 1 && (
-                            <Button variant="success" onClick={handleAddRow}>+</Button>
-                        )}
-                    </Col>
-                </Row>
+                <FunderRow  row={row} index={index} rows={rows} setRows={setRows} handleAddRow={handleAddRow} />
             ))}
         </div>
+    );
+};
+
+const FunderRow = (props) => {
+    const { row, index, rows, setRows, handleAddRow } = props;
+    const { frmData, updateFormData } = useContext(PublicationFormContext);
+
+    const roleOptions = [
+        {
+            value: "associate professor",
+            label: "Associate Professor"
+        }
+    ];
+    const handleInputChange = (value, fieldName, rowIndex) => {
+        const updatedRows = [...rows];
+        updatedRows[rowIndex][fieldName] = value;
+        setRows(updatedRows);
+
+        const org = [...frmData.funders];
+        org[index] = rows[index];
+        if (fieldName === "type") {
+            org[index].type = org[index].type.value;
+        }
+        updateFormData("funders", org);
+    }
+
+    return (
+
+
+        <Row key={index} className="m-3">
+            <Col>
+                <Select
+                    options={[{ value: 'funder', label: 'AddFunder' }, { value: 'grantor', label: 'Grantor' }]}
+                    placeholder="Select Type"
+                    onChange={(selectedOption) => handleInputChange(selectedOption, 'type', index)}
+                />
+            </Col>
+            <Col>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter Name"
+                    value={row.name}
+                    onChange={(e) => handleInputChange(e.target.value, 'name', index)}
+                />
+            </Col>
+            <Col>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter ID"
+                    value={row.id}
+                    onChange={(e) => handleInputChange(e.target.value, 'id', index)}
+                />
+            </Col>
+            <Col>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter Activity"
+                    value={row.activity}
+                    onChange={(e) => handleInputChange(e.target.value, 'activity', index)}
+                />
+            </Col>
+            <Col>
+                {index === rows.length - 1 && (
+                    <Button variant="success" onClick={handleAddRow}>+</Button>
+                )}
+            </Col>
+        </Row>
     );
 };
 
--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from "react";
 import { Form, Row, Col, Button } from 'react-bootstrap';
+import { PublicationFormContext } from "./context/PublicationFormContext.jsx";
+import Select from "react-select";
 
 const AddProject = ({ onAddProject }) => {
     const [rows, setRows] = useState([{ title: '', description: '' }]);
@@ -17,32 +19,58 @@ const AddProject = ({ onAddProject }) => {
     return (
         <div>
             {rows.map((row, index) => (
-                <Row key={index} className="m-3 align-items-center">
-                    <Col>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter project title"
-                            value={row.title}
-                            onChange={(e) => handleInputChange(e.target.value, 'title', index)}
-                        />
-                    </Col>
-                    <Col>
-                        <Form.Control
-                            as="textarea"
-                            rows={3}
-                            placeholder="Enter project description"
-                            value={row.description}
-                            onChange={(e) => handleInputChange(e.target.value, 'description', index)}
-                        />
-                    </Col>
-                    <Col xs="auto">
-                        {index === rows.length - 1 && (
-                            <Button variant="success" onClick={handleAddRow}>+</Button>
-                        )}
-                    </Col>
-                </Row>
+                <ProjectRow  row={row} index={index} rows={rows} setRows={setRows} handleAddRow={handleAddRow} />
             ))}
         </div>
+    );
+};
+
+const ProjectRow = (props) => {
+    const { row, index, rows, setRows, handleAddRow } = props;
+    const { frmData, updateFormData } = useContext(PublicationFormContext);
+
+    const roleOptions = [
+        {
+            value: "associate professor",
+            label: "Associate Professor"
+        }
+    ];
+    const handleInputChange = (value, fieldName, rowIndex) => {
+        const updatedRows = [...rows];
+        updatedRows[rowIndex][fieldName] = value;
+        setRows(updatedRows);
+
+        const org = [...frmData.projects];
+        org[index] = rows[index];
+        updateFormData("projects", org);
+    }
+
+    return (
+
+        <Row key={index} className="m-3 align-items-center">
+            <Col>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter project title"
+                    value={row.title}
+                    onChange={(e) => handleInputChange(e.target.value, 'title', index)}
+                />
+            </Col>
+            <Col>
+                <Form.Control
+                    as="textarea"
+                    rows={3}
+                    placeholder="Enter project description"
+                    value={row.description}
+                    onChange={(e) => handleInputChange(e.target.value, 'description', index)}
+                />
+            </Col>
+            <Col xs="auto">
+                {index === rows.length - 1 && (
+                    <Button variant="success" onClick={handleAddRow}>+</Button>
+                )}
+            </Col>
+        </Row>
     );
 };
 
