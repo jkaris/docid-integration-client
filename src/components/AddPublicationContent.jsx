4a213f8f99d0629dd673311ffc8de2008e872294
Joseph Kariuki <joehene@gmail.com>
2024-03-25 04:41:45 +0300

    Refactored add publication logic

--- src/components/AddPublicationContent.jsx
+++ src/components/AddPublicationContent.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Form, Button, Container, Row, Col, FloatingLabel } from "react-bootstrap";
-import Select from "react-select";
+import { Form } from "react-bootstrap";
 import { PublicationControl } from "./addfile/PublicationControl.jsx";
 import { PublicationFormContext } from "./context/PublicationFormContext.jsx";
 
@@ -10,14 +9,11 @@ const AddPublicationContent = () => {
     const [metadata, setMetadata] = useState({ title: "", description: "", mediaType: "" });
     const handleFileChange = (e) => {
         const selectedFiles = Array.from(e.target.files);
-        // setFiles([...files, ...selectedFiles.map(file => ({ file, ...metadata }))]);
         setFiles([...files, ...selectedFiles]);
-        // console.log("files zenye ziko", files)
-        // window.document.getElementById("selectPubDocs").textContent = `${files.length} Documents`
     };
     useEffect(() => {
         const fl = files.map(data => ({file: data.file}))
-        setCtxFiles(prevFiles=>({...prevFiles, ["publications"]: fl}))
+        setCtxFiles(prevFiles=>({...prevFiles, ["Publication"]: fl}))
     }, [files]);
 
 
@@ -28,53 +24,6 @@ const AddPublicationContent = () => {
         frmData.funders = [];
     }, []);
 
-    // useEffect(() => {
-    //     frmData.publications = files.map(f => ({ title: f.title, description: f.description, mediaType: f.mediaType }));
-    //     updateFormData("publications", frmData.publications);
-    // }, [files]);
-    //
-    // const handleMetadataChange = (e, index) => {
-    //     const { name, value } = e.target;
-    //     const updatedFiles = [...files];
-    //     updatedFiles[index][name] = value;
-    //     setFiles(updatedFiles);
-    // };
-    //
-    // const handleRemoveFile = (index) => {
-    //     const updatedFiles = [...files];
-    //     updatedFiles.splice(index, 1);
-    //     setFiles(updatedFiles);
-    // };
-    //
-    // const handleMediaTypeChange = (selectedOption) => {
-    //     setSelectedMediaType(selectedOption);
-    // };
-    //
-    // const handleIdentifierTypeChange = (selectedIdentifierType) => {
-    //     setSelectedIdentifierType(selectedIdentifierType);
-    // };
-    //
-    // const handleGenerateId = async (index) => {
-    //     const { mediaType } = files[index];
-    //     if (mediaType === "patent") {
-    //         console.log("Generating DOI for patent");
-    //     } else if (mediaType === "webinar") {
-    //         console.log("Generating Handles for webinar");
-    //     }
-    // };
-
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     const formData = new FormData();
-    //     files.forEach(({ file, ...metadata }) => {
-    //         formData.append("files", file);
-    //         Object.entries(metadata).forEach(([key, value]) => {
-    //             formData.append(`${file.name}_${key}`, value);
-    //         });
-    //     });
-    //     console.log("FormData:", formData);
-    // };
-
     return (
         <>
             <Form.Group>
@@ -83,7 +32,7 @@ const AddPublicationContent = () => {
             </Form.Group>
             {files.map((fileProps, index) => (
                 <PublicationControl key={index} files={files} setFiles={setFiles} fileProps={fileProps} idx={index}
-                                    docType={"publications"} />
+                                    docType={"Publication"} />
             ))}
         </>
     );
